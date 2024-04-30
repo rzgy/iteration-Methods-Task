e918e65a75ccc7af2b269ef3572f1fd2c69dda94
Abdulrazak <rzookalq@gmail.com>
2024-04-30 19:55:56 +0300

    all done except the bonus

---
+++ index.js
@@ -0,0 +1,118 @@
+//You are given an array of numbers. Your task is to use iteration methods to filter and transform this array.
+
+const numbers = [10, 13, 20, 25, 38, 35, 40];
+
+/*****************************************************************
+Part 1: Filtering
+
+Write code to perform the following tasks using the filter method:
+Q1) Create a new array containing numbers that are greater than or equal to 25.
+Q2) Create a new array containing numbers that are divisible by 5.
+******************************************************************/
+
+const result = numbers.filter((num) => {
+  return num >= 25;
+});
+console.log(result);
+
+const divisibleNum = numbers.filter((num) => {
+  return num % 5 == 0;
+});
+console.log(divisibleNum);
+
+/*****************************************************************
+Part 2: Mapping
+
+Write code to perform the following tasks using the map method:
+
+Q3) Create a new array that contains each number squared.
+Q4) Create a new array that contains each number multiplied by 2.
+******************************************************************/
+
+const squaredNum = numbers.map((num) => {
+  return num ** 2;
+});
+console.log(squaredNum);
+
+const multiplied = numbers.map((num) => {
+  return num * 2;
+});
+console.log(multiplied);
+
+/*****************************************************************
+Part 3: Combining Filtering and Mapping
+
+Combine the filter and map methods to perform the following tasks:
+
+Q5) Filter the numbers that are greater than or equal to 20 and then square each of them.
+Q6) Filter the numbers that are divisible by 5 and then multiply each of them by 3.
+******************************************************************/
+
+const bigger = numbers.filter((num) => {
+  return num >= 20;
+});
+
+const res = bigger.map((num) => {
+  return num ** 2;
+});
+
+console.log(res);
+
+const divisibleNumy = numbers.filter((num) => {
+  return num % 5 == 0;
+});
+
+const resu = divisibleNumy.map((num) => {
+  return num * 3;
+});
+
+console.log(resu);
+/*****************************************************************
+Part 4: Extra not required 🌶️🌶️🌶️
+******************************************************************/
+/*************************************
+ * logger(array)
+ *
+ * - Accepts an array
+ * - Logs every element of the array
+ ************************************/
+const logger = function (array) {};
+
+/*************************************
+ * toCelsius(temperatures)
+ *
+ * - Accepts an array of temperatures
+ *   in degrees Fahrenheit
+ * - Returns an array of temperatures
+ *   in degrees Celsius
+ *
+ * The conversion is:
+ *   C = (F - 32) * (5/9)
+ ************************************/
+const toCelsius = function (temperatures) {};
+
+/**************************************
+ * hottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ * - Accepts a threshhold temperature
+ * - Returns an array of temperatures
+ *   that exceed the threshhold
+ ***************************************/
+const hottestDays = function (temperatures, threshhold) {};
+
+/******************************************
+ * logHottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ *   IN DEGREES FAHRENHEINT
+ * - Accepts a threshhold temperature
+ *   IN DEGREES FAHRENHEINT
+ * - Logs temperatures that exceed the
+ *   threshhold to the console
+ *   IN DEGREES CELSIUS
+ *
+ * hint: you can combine
+ *       all previous functions
+ *******************************************/
+const logHottestDays = function (temperatures, threshhold) {};
